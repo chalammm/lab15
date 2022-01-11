fa92c8fe3c17ccb927c357544a6b60c330229134
chalammm <tipanan_ji@cmu.ac.th>
2022-01-11 16:44:09 +0700

    completedlab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+    int i,j; 
+	double z;
+    
+    for (i = 1; i < N; i++)
+    { 
+        z = d[i]; 
+        j = i - 1; 
 
+        while (j >= 0 && d[j] < z)
+        { 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = z; 
+        cout << "Pass " << i << ":";
+        for (int x = 0; x < 10; x++) cout << d[x] << " ";
+        cout << "\n";
+    } 
 }
 
 int main(){
